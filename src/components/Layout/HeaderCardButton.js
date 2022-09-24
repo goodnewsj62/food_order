231791d74df6d8329d6e8a3171e7d4addf907f91
goodnews <goodnewsj62@gmail.com>
2022-09-24 09:48:20 +0100

    finish

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -2,16 +2,37 @@ import CartIcon from "./CartIcon";
 import CartContext  from "store/cart_data"
 import styles from "./HeaderCartButton.module.css";
 import { useContext } from "react";
+import { useEffect, useState } from "react";
 
 const HeaderCartButton =  (props) =>{
     const ItemContext = useContext(CartContext);
+    const [btnAnime, setBtnAnime]  = useState(false);
+    
+    const {items } = ItemContext;
+    
+    useEffect(()=>{
+        if(ItemContext.items.length === 0){
+            return;
+        }
+        setBtnAnime(true);
 
-    const numberOfCartItems =  ItemContext.items.reduce((currentNum, item)=>{
+        const timer =  setTimeout(()=>{
+            setBtnAnime(false);
+        }, 300)
+
+        return ()=>{
+            clearTimeout(timer);
+        };
+    }, [items])
+
+    const numberOfCartItems =  items.reduce((currentNum, item)=>{
         return currentNum +  item.amount
     },0);
 
+    const btnClasses =  `${styles.button} ${btnAnime && styles.bump}`
+
     return (
-        <button onClick={props.onClick} className={styles.button}>
+        <button onClick={props.onClick} className={btnClasses}>
             <span className={styles.icon}>
                 <CartIcon />
             </span>
